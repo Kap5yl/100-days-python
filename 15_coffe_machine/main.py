bc22fb1e886bad9af045c686dc1024dbd3042590
Kap5yl <94987643+Kap5yl@users.noreply.github.com>
2021-11-24 20:02:20 +0100

    Add files via upload

---
+++ 15_coffe_machine/main.py
@@ -0,0 +1,74 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+def coins():
+    print("input money")
+    money = int(input("how many quarters: ")) * 0.25
+    money += int(input("how many dimes: ")) * 0.1
+    money += int(input("how many nickels: ")) * 0.05
+    money += int(input("how many pennies: ")) * 0.01
+    return money
+
+def check_resourses(sum_ingredients):
+    for n in sum_ingredients:
+        if sum_ingredients[n] >= resources[n]:
+            print("cant make it")
+            return 0
+    return 1
+
+income = 0
+on = 1
+
+while on:
+    choice = input("what do you want? espresso, latte, cappucino: ")
+    if choice == "off":
+        on = 0
+    elif choice == "report":
+        print(f"water: {resources['water']}ml")
+        print(f"milk: {resources['milk']}ml")
+        print(f"coffe: {resources['coffee']}g")
+        print(f"money: ${income}")
+    else:
+        drink = MENU[choice]
+        if check_resourses(drink["ingredients"]):
+            pay = coins()
+            if pay < drink["cost"]:
+                print("no money")
+            else:
+                income = drink["cost"]
+                print(f"Here is change: {round(pay - drink['cost'], 2)}")
+                for n in drink["ingredients"]:
+                    resources[n] -= drink["ingredients"][n]
+ 
+            
+            
+        
